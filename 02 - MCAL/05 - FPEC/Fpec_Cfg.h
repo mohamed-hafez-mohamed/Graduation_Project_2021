dd4473d9c83c0bb09d21da4b1f51122c7d7055a8
xMosad <53371999+xMosad@users.noreply.github.com>
2021-04-29 08:29:44 +0200

    Add files via upload

---
+++ 02 - MCAL/05 - FPEC/Fpec_Cfg.h
@@ -0,0 +1,15 @@
+/*****************************************************/
+ /* Author      : mosad                              */
+ /* Version     : v01                                */
+ /* date        : 9/10/2020                          */
+/*****************************************************/
+#ifndef FPEC_CONFIG_H
+#define FPEC_CONFIG_H
+
+
+#define     FPEC_FLASH_START_ADDRESS      0x08000000
+#define     FPEC_FLASH_PAGE_SIZE          1024
+
+
+
+#endif
