0a4f0a37b9e7460940b4789a998a58bc5bb8468a
osama hijazi <osamahegazi@students.mans.edu.eg>
2021-05-07 01:25:39 +0200

    can.cfg modify

--- 02 - MCAL/06 - CAN/CAN_cfg.c
+++ 02 - MCAL/06 - CAN/CAN_cfg.c
@@ -1,11 +1,11 @@
 /*******************************************************************************
-**  FILENAME     : Can_cfg.c         			                                        **
+**  FILENAME     : Can_cfg.c         			                              **
 **                                                                            **
 **  VERSION      : 1.2.0                                                      **
 **                                                                            **
 **  DATE         : 2021-04-28                                                 **
 **                                                                            **
-**  PLATFORM     : stm32f103		                                              **
+**  PLATFORM     : stm32f103		                                          **
 **                                                                            **
 **  AUTHOR       : osamahijazi	                                              **
 **                                                                            **                                                                           **                                                                          **
@@ -16,10 +16,11 @@
 #include "CAN_Interface.h"
 
 
-/*FOR BIT RATE 500KHZ , PRESCALE = 1, SJW = 0 ,SEG1 = 11 , SEG2 = 4 , SAMPLE PIONT AT 87.5 , BTR=  0x00140001 */	 
+/*FOR BIT RATE 500KHZ , PRESCALE = 1, SJW = 0 ,SEG1 = 0X0C , SEG2 = 0X01 , SAMPLE PIONT AT 87.5 , BTR=  0x001C0000 */	 
  
                         /* they are not stabdard containers but keep it now until new version become tested and safe :)*/
-CAN_InitTypeDef        CAN_InitStruct         =	{ 1 , CAN_Mode_Normal   , 0 , 11 , 4, DISABLE , DISABLE , DISABLE , DISABLE , DISABLE , DISABLE } ;
+CAN_InitTypeDef        CAN_InitStruct         =	{ 1 , CAN_Mode_Normal   , 0 , 0X0C , 0X01 , DISABLE , DISABLE , DISABLE , DISABLE , DISABLE , DISABLE } ;
+
 
 CAN_FilterInitTypeDef  CAN_FilterInitStruct_1 = { 0x00600000 , 0x00600000 , CAN_FilterMode_IdList ,  1  , CAN_FilterScale_32bit , CAN_Filter_FIFO0 , ENABLE}; // ID = 0X03 
 
