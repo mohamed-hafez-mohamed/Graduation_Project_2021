878e368c1579a192ba7f63df16daa9d8fefab291
mosad <abdelrhman.m0s3d@gmail.com>
2021-05-28 16:08:32 +0200

    RTE update

--- 02- Software/01 - Dash Board System/05 - RTE/Rte_DataTypes.h
+++ 02- Software/01 - Dash Board System/05 - RTE/Rte_DataTypes.h
@@ -12,5 +12,24 @@ typedef     uint8    FlagType ;
 #define    FLAG_NOT_SET              0x0u
 #define    FLAG_SET                  0x1u
 
+/* Time Array Index */
+#define    RTC_SECONDS_INDEX      0
+#define    RTC_MINUTES_INDEX      1
+#define    RTC_HOURS_INDEX        2
+#define    RTC_DAY_INDEX          3
+#define    RTC_DATE_INDEX         4
+#define    RTC_MONTH_INDEX        5
+#define    RTC_YEAR_INDEX         6
+
+typedef   uint8    RtcDayType ;
+/* Day */
+#define     RTE_MONDAY     0x01
+#define     RTE_TUESDAY    0x02
+#define     RTE_WEDNSDAY   0x03
+#define     RTE_THURSDAY   0x04
+#define     RTE_FRIDAY     0x05
+#define     RTE_SATURDAY   0x06
+#define     RTE_SUNDAY     0x07
+
 
 #endif
